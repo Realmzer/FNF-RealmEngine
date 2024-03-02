218511be868400a05f6279b018c1168d54fbd744
Realmzer <therealm4mc@gmail.com>
2024-03-01 20:09:27 -0500

    Funny stuff

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -39,6 +39,9 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Realm Engine Team'],
+			['Realmzer',            'realmzer',         'Main Programmer of Realm engine',                           'https://twitter.com/Realmzer',        'C06FFF'],
+			[''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer and Head of Psych Engine',					 'https://ko-fi.com/shadowmario',		'444444'],
 			['Riveren',				'riveren',			'Main Artist/Animator of Psych Engine',						 'https://twitter.com/riverennn',		'14967B'],
--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -87,14 +87,10 @@ class MainMenuState extends MusicBeatState
 			menuItem.screenCenter(X);
 		}
 
-		var realmver:FlxText = new FlxText(12, FlxG.height - 64, 0, "Realm Engine" + realmEngineVersion, 12);
+		var realmver:FlxText = new FlxText(12, FlxG.height - 44, 0, "Realm Engine" + realmEngineVersion, 12);
 		realmver.scrollFactor.set();
 		realmver.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(realmver);
-		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
-		psychVer.scrollFactor.set();
-		psychVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(psychVer);
 		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
 		fnfVer.scrollFactor.set();
 		fnfVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
